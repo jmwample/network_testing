7757e85a357e93859df7f09152f56a90e54614cc
Jack Wampler <jack.wampler@colorado.edu>
2018-02-07 15:38:56 -0700

    Initial commit Testing Different Versions of Echo Server for TCP/TLS conections

---
+++ echo1.go
@@ -0,0 +1,58 @@
+package main
+
+// $ 6g echo.go && 6l -o echo echo.6
+// $ ./echo
+//
+//  ~ in another terminal ~
+//
+// $ nc localhost 3540
+
+
+import (
+	"net"
+	"bufio"
+	"strconv"
+	"fmt"
+)
+
+const PORT = 16667
+
+func main() {
+    server, err := net.Listen("tcp", ":" + strconv.Itoa(PORT))
+    if server == nil {
+        panic("couldn't start listening: " + err.Error())
+    }
+    conns := clientConns(server)
+    for {
+        go handleConn(<-conns)
+    }
+}
+
+func clientConns(listener net.Listener) chan net.Conn {
+    ch := make(chan net.Conn)
+    i := 0
+    go func() {
+        for {
+            client, err := listener.Accept()
+            if client == nil {
+                fmt.Printf("couldn't accept: " + err.Error())
+                continue
+            }
+            i++
+            fmt.Printf("%d: %v <-> %v\n", i, client.LocalAddr(), client.RemoteAddr())
+            ch <- client
+        }
+    }()
+    return ch
+}
+
+func handleConn(client net.Conn) {
+    b := bufio.NewReader(client)
+    for {
+        line, err := b.ReadBytes('\n')
+        if err != nil { // EOF, or worse
+            break
+        }
+        client.Write(line)
+    }
+}
